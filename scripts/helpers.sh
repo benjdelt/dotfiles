8be79968205f7e18bdb9f1a2be48eef5bcb60e61
Ben Deltenre <benjdelt@gmail.com>
2020-05-20 16:54:33 -0700

    Added helper functions

---
+++ scripts/helpers.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+readonly BOLD='\033[1m'
+readonly RED='\033[1;31m'
+readonly RED_BOLD='\033[1;91m'
+readonly GREEN='\033[1;32m'
+readonly GREEN_BOLD='\033[1;92m'
+readonly BLUE='\033[1;34m'
+readonly BLUE_BOLD='\033[1;94m'
+readonly YELLOW='\033[1;33m'
+readonly YELLOW_BOLD='\033[1;93m'
+
+# All scripts
+
+# Messages
+
+msg_info () {
+    echo -e "${BLUE}$1${RESET_FORMAT}"
+}
+
+msg_success () {
+    echo -e "${GREEN}$1${RESET_FORMAT}"
+}
+
+msg_warning () {
+    echo -e "${YELLOW}$1${RESET_FORMAT}"
+}
+
+msg_alert () {
+    echo -e "${RED_BOLD}$1${RESET_FORMAT}"
+}
+
+# Install scripts
+
+backup_file () {
+    rsync -rv --no-links --ignore-existing "$1" "$2"
+}
+
+remove_existing_dir () {
+    local basename
+    basename=$(basename "$1")
+    local target_dir=$HOME/$basename
+    if [[ -d $1 && -d $target_dir ]]; then
+	echo "target $target_dir"
+	rm -rf "$target_dir"
+    fi
+}
+
+make_link () {
+    ln -svf "$1" "$2"
+}
+
+# Restore scripts
+
+restore_file () {
+    cp -frv "$1" "$2"
+}
+
+# Main scripts
+
+handle_missing_option () {
+    if (("$1" == 1)); then
+        echo 'Missing option' 1>&2
+        echo "Try './scripts/install -h' for more information" 1>&2
+        exit 1
+    fi
+}
+
+handle_invalid_arg () {
+    if [[ "$1" -gt  0 ]]; then
+        echo "Unrecognized argument(s): $2" 1>&2
+        echo "Try './scripts/install -h' for more information" 1>&2
+        exit 1
+    fi
+}
+
